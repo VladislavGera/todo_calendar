5bd81e145d5a3eb691c18e022ef02b53ca006b56
login <vheranin@gmail.com>
2022-10-27 14:15:04 +0300

    add typeing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { SelectorComponent } from './components/selector/selector.component';
-import { OranizerComponent } from './components/organizer/oranizer.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialExampleModule } from '../../src/material.module';
-import { SharedModule } from './shared/shared.module';
+
 import { CoreModule } from './components/core.component.module';
 
 @NgModule({
